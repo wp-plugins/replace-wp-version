f62922bdd7da3b523a1a25b7523676a6fa3de14e
Bueltge <Bueltge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-13 13:46:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/replace-wp-version/trunk@31740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fb-re_wpversion.php
+++ fb-re_wpversion.php
@@ -4,31 +4,38 @@ Plugin Name: Replace WP-Version
 Plugin URI: http://bueltge.de/wordpress-version-verschleiern-plugin/602/
 Description: Replace the WP-version with a random string &lt; WP 2.4 and eliminate WP-version &gt; WP 2.4
 Author: Frank Bueltge
-Version: 0.1
+Version: 0.2
 License: GPL
 Author URI: http://bueltge.de/
 */
 
-if (!is_admin() && !is_archive() && !is_attachment() && !is_author() &&
-		!is_category() && !is_comments_popup() && !is_date() && !is_day() &&
-		!is_home() && !is_month() && !is_page() && !is_paged() && !is_preview() &&
-		!is_robots() && !is_search() && !is_single() && !is_singular() &&
-		!is_time() && !is_trackback() && !is_year() && !is_404() ) {
-	
-	// random value
-	$v = intval( rand(0, 9999) );
-	
-	if ( function_exists('the_generator') ) {
-		// eliminate version for wordpress >= 2.4
-		add_filter( 'the_generator', create_function('$a', "return null;") );
-		// add_filter( 'wp_generator_type', create_function( '$a', "return null;" ) );
+function fb_replace_wp_version() {
+
+	if ( !is_admin() ) {
+		global $wp_version;
 		
-	} else {
-		// for wordpress < 2.4
-		add_filter( "bloginfo_rss('version')", create_function('$a', "return $v;") );
+		// random value
+		$v = intval( rand(0, 9999) );
 		
-		// for rdf and rss v0.92
-		$wp_version = $v;
+		if ( function_exists('the_generator') ) {
+			// eliminate version for wordpress >= 2.4
+			add_filter( 'the_generator', create_function('$a', "return null;") );
+			// add_filter( 'wp_generator_type', create_function( '$a', "return null;" ) );
+			
+			// for $wp_version
+			$wp_version = $v;
+		} else {
+			// for wordpress < 2.4
+			add_filter( "bloginfo_rss('version')", create_function('$a', "return $v;") );
+			
+			// for rdf and rss v0.92
+			$wp_version = $v;
+		}
 	}
+
+}
+
+if ( function_exists('add_action') ) {
+	add_action('init', fb_replace_wp_version, 1);
 }
 ?>
